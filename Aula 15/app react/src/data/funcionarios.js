83291515223e65306e12a8523409f8d1227d73ee
marcosVCarvalho118 <marcos.vinnisc@gmail.com>
2026-05-23 17:03:41 -0300

    Enhance React notes and update employee images
    Expanded and reorganized Aula 15 notes with clearer React explanations, step-by-step setup instructions, component/props/map/return details, and minor typo fixes. Also updated sample employee data in funcionarios.js to use real image URLs for entries (Diego and Maria).

--- Aula 15/app react/src/data/funcionarios.js
+++ Aula 15/app react/src/data/funcionarios.js
@@ -1,12 +1,13 @@
 const funcionarios = [
     {
-      img:'https://placehold.co/100x100?text=DC',
+      img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UV35BLbf9yg6VdM8kzkk8mghz_wNfeyyKg&s',
       nome: 'Diego Cezar',
       cargo: 'Instrutor Senac RJ',
       bio: 'Laura é uma criança',
       habilidade:['JavaScript', 'css']
     },
     {
+      img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UV35BLbf9yg6VdM8kzkk8mghz_wNfeyyKg&s',
       nome: 'Maria Silva',
       cargo: 'Desenvolverdora Front-end',
       bio: 'Maria está zangada',
